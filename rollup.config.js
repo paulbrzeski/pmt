baff9ae4f0dd36efa6b4b9ad1bcbec50d117f825
Paul Brzeski <mail@paulbrzeski.com>
2018-12-05 19:59:16 +0800

    Initial seed with kettlefish

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import buble from 'rollup-plugin-buble';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  input: 'src/app.js',
+  output: {
+  	file: 'dist/app.js',
+  	format: 'cjs'
+  },
+  plugins: [
+    buble(),
+    uglify()
+  ]
+};
